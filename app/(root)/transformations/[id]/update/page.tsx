c1c97b1777865d29be1521035bab07cdadc3c47a
pritika mishra <pritikamishra2017@gmail.com>
2024-07-08 17:47:41 +0530

    full app done

--- app/(root)/transformations/[id]/update/page.tsx
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from "next/navigation";
 
-const UpdateTranformationsPage = () => {
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.action";
+import { getImageById } from "@/lib/actions/image.actions";
+
+const Page = async ({ params: { id } }: SearchParamProps) => {
+    const user = await currentUser()
+
+  if (!user?.id) redirect("/sign-in");
+
+  const dbUser = await getUserById(user?.id)
+  const image = await getImageById(id);
+
+  const transformation =
+    transformationTypes[image.transformationType as TransformationTypeKey];
+
   return (
-    <div>UpdateTranformationsPage</div>
-  )
-}
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
 
-export default UpdateTranformationsPage
+      <section className="mt-10">
+        <TransformationForm
+          action="Update"
+          userId={dbUser._id}
+          type={image.transformationType as TransformationTypeKey}
+          creditBalance={dbUser.creditBalance}
+          config={image.config}
+          data={image}
+        />
+      </section>
+    </>
+  );
+};
+
+export default Page;
--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,9 +1,36 @@
+import Header from '@/components/shared/Header'
 import React from 'react'
+import { transformationTypes } from '@/constants'
+import TransformationForm from '@/components/shared/TransformationForm'
+import { getUserById } from '@/lib/actions/user.action'
+import { redirect } from 'next/navigation'
+import { currentUser } from '@clerk/nextjs/server'
 
-const AddTranformationsTypePage = () => {
+const AddTransformationsTypePage = async ({ params: { type } }: SearchParamProps) => {
+  const user = await currentUser()
+
+  if (!user) redirect('/sign-in')
+
+  const dbUser = await getUserById(user.id)
+  const transformation = transformationTypes[type]
+
   return (
-    <div>AddTranformationsTypePage</div>
+    <>
+      <Header
+        title={transformation.title}
+        subtitle={transformation.subTitle}
+      />
+
+      <section className='mt-10'>
+        <TransformationForm
+          action='Add'
+          userId={dbUser._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={dbUser.creditBalance}
+        />
+      </section>
+    </>
   )
 }
 
-export default AddTranformationsTypePage
+export default AddTransformationsTypePage
